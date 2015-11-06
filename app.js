f2d5bc7bf5bfb0cb942c8a156ce53cf625dbcbea
xiaonengmiao <1123913594@qq.com>
2015-11-06 19:03:47 +0800

    First commit

---
+++ app.js
@@ -0,0 +1,71 @@
+var main = function() {
+    $('.header').load("menubar.htm");
+    $('.dropdown-toggle').click(function() {
+                                $('.dropdown-menu').toggle();
+                                });
+    
+    
+    $('.arrow-next').click(function() {
+                           var currentSlide = $('.active-slide');
+                           var nextSlide = currentSlide.next();
+                           
+                           var currentDot = $('.active-dot');
+                           var nextDot = currentDot.next();
+                           
+                           if(nextSlide.length === 0) {
+                           nextSlide = $('.slide').first();
+                           nextDot = $('.dot').first();
+                           }
+                           
+                           currentSlide.fadeOut(600).removeClass('active-slide');
+                           nextSlide.fadeIn(600).addClass('active-slide');
+                           
+                           currentDot.removeClass('active-dot');
+                           nextDot.addClass('active-dot');
+                           });
+    
+    
+    $('.arrow-prev').click(function() {
+                           var currentSlide = $('.active-slide');
+                           var prevSlide = currentSlide.prev();
+                           
+                           var currentDot = $('.active-dot');
+                           var prevDot = currentDot.prev();
+                           
+                           if(prevSlide.length === 0) {
+                           prevSlide = $('.slide').last();
+                           prevDot = $('.dot').last();
+                           }
+                           
+                           currentSlide.fadeOut(600).removeClass('active-slide');
+                           prevSlide.fadeIn(600).addClass('active-slide');
+                           
+                           currentDot.removeClass('active-dot');
+                           prevDot.addClass('active-dot');
+                           });
+    
+    var myName = "xiaonengmiao";
+    
+    var red = [0, 100, 63];
+    var orange = [40, 100, 60];
+    var green = [75, 100, 40];
+    var blue = [196, 77, 55];
+    var purple = [280, 50, 60];
+    var letterColors = [red, orange, green, blue, purple];
+    
+    drawName(myName, letterColors);
+    
+    if(10 < 3)
+    {
+        bubbleShape = 'square';
+    }
+    else
+    {
+        bubbleShape = 'circle';
+    }
+    
+    bounceBubbles();
+    
+}
+
+$(document).ready(main);
